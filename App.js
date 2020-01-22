e3da1077207d84b917f57f070a96346ba02a0478
bkalafat <kalafatburak@gmail.com>
2020-01-22 14:25:19 +0300

    style: Align items and justify content center

--- App.js
+++ App.js
@@ -4,16 +4,6 @@ import DeckList from './components/DeckList'
 
 export default function App() {
   return (
-    <View style={styles}><DeckList /></View>
-
+    <DeckList />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,12 +1,22 @@
-import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class DeckList extends Component {
   render() {
     return (
-      <View>
-        <Text>DeckList dev</Text>
+      <View style={styles.container} >
+        <Text  >DeckList dev</Text>
       </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
