672a6d605cd5cdbf36f1d6656aea3be6b269daa6
bkalafat <kalafatburak@gmail.com>
2020-02-16 12:17:03 +0300

    feat: Add deck view.

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import DeckList from './components/DeckList'
 import NewDeck from './components/NewDeck'
+import DeckView from './components/DeckView'
+import Constants from 'expo-constants'
+import { purple } from './utils/colors';
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <NewDeck />
+    <View style={{ flex: 1 }} >
+      <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+      <DeckView deckName="udacicards" cardCount="3" />
+    </View>
   );
 }
